068e25a42b6c9a32c5ebc2b622ffb5934f02f264
Joao Carvalho <joao.carvalho@sinqia.com.br>
2020-03-10 19:30:06 -0300

    Test

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("View.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
