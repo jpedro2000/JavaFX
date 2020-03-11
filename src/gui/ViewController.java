49c1688f4285bbcc9935f2957f2026e1f48bae10
Joao Carvalho <joao.carvalho@sinqia.com.br>
2020-03-11 19:39:17 -0300

    Adicionando novas operações

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,15 +1,19 @@
 package gui;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class ViewController {
+public class ViewController implements Initializable {
 
     @FXML
     private TextField txtNumber1;
@@ -24,6 +28,12 @@ public class ViewController {
     private Button btSum;
 
     @FXML
+    private Button btSub;
+
+    @FXML
+    private Button btMult;
+
+    @FXML
     public void onBtSumAction(){
         try {
             Locale.setDefault(Locale.US);
@@ -36,4 +46,33 @@ public class ViewController {
         }
     }
 
+    public void onBtSubAction(){
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sub = number1 - number2;
+            labelResult.setText(String.format("%.2f", sub));
+        }catch (NumberFormatException e){
+            Alerts.showAlert("Error", "parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    public void onBtMultAction(){
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double mult = number1 * number2;
+            labelResult.setText(String.format("%.2f", mult));
+        }catch (NumberFormatException e){
+            Alerts.showAlert("Error", "parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Constraints.setTextFieldDouble(txtNumber1);
+        Constraints.setTextFieldDouble(txtNumber2);
+    }
 }
