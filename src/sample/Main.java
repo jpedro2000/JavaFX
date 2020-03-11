71718a07f3df6c13ef2265d645f4dcb42350d7cc
Joao Carvalho <joao.carvalho@sinqia.com.br>
2020-03-11 19:04:40 -0300

    Atribuindo funcionalidades e criando alertas

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,25 @@
 package sample;
 
+import gui.util.Alerts;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
-import javafx.scene.layout.BorderPane;
-
-import java.io.IOException;
+import java.io.EOFException;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) {
-        try {
-            Parent parent = FXMLLoader.load(getClass().getResource("View.fxml"));
-            Scene scene = new Scene(parent);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+            primaryStage.setTitle("Calculator");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
